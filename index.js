4b362c966332e47b4cd1992beeb2b9036f8f9a9e
Jonathan Kulak <lackadaize@msn.com>
2024-06-12 11:16:44 -0400

    This is the Static Methods Lab assignment.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, "")
+  }
+
+  static titleize(string){
+    const nonCapitals = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    const words = string.toLowerCase().split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !nonCapitals.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+    }
 }
+
+// console.log(Formatter.capitalize("hello world!"));
+// console.log(Formatter.sanitize("hello world!"));
+// console.log(Formatter.titleize("LORD OF THE RINGS"));
